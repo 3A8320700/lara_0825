f4c1827c72f00430a1bd2baca30c15d824e04cba
LAPTOP-Q4E3DFIH\s9633 <s3a832070@student.ncut.edu.tw>
2021-11-01 19:16:45 +0800

    新增posts, show, about, contact路由，串接PostsController的函數

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('contact');
+    return view('welcome');
 });
+
+Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
+Route::get('post', [\App\Http\Controllers\PostsController::class, 'show'])->name('posts.show');
+Route::get('contact', [\App\Http\Controllers\PostsController::class, 'contact'])->name('posts.contact');
+Route::get('about', [\App\Http\Controllers\PostsController::class, 'about'])->name('posts.about');
