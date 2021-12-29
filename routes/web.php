a005cc95634b29fe1db6f775d5e5b18bdaae9010
LAPTOP-Q4E3DFIH\s9633 <s3a832070@student.ncut.edu.tw>
2021-12-29 10:24:19 +0800

    練習 5:刪除資料

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
-
 use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
@@ -15,42 +13,52 @@ use App\Models\Post;
 |
 */
 Route::get('/', function () {
+    /* 新增資料
     $post=new Post();//先產生Post的物件$post，$post代表資料表posts的一篇貼文
     $post->title='test title';//指定貼文的title
     $post->content='test content';//指定貼文的content
     $post->save();
-//使用create方法，P.S 應該會遇到 Mass Assignment 的警告
+    //使用create方法，P.S 應該會遇到 Mass Assignment 的警告
     Post::create([
         'title'=>'test title',
         'content'=>'test content',
     ]);
-//使用all方法
+    */
+    /* 查詢資料
+    //使用all方法
     $posts = Post::all();//取出posts資料表所有貼文
     dd($posts);//dd=data dump，將變數內容倒出，並停止程式執行
-//使用find方法
+    //使用find方法
     $post = Post::find(11);//找尋posts資料表id=1的貼文
     dd($post);
-//練習條件式
+    //練習條件式
     $posts = Post::where('id','>',10)->orderby('id','DESC')->get();//查詢符合條件的貼文，排序後取出
     dd($posts);
-//return view('welcome');
-//使用update方法
-    $post = Post::find(1);
-    $post -> update([
+    */
+    /* 更新資料
+    //使用update方法
+    $post = Post::find(11);
+    $post->update([
         'title'=>'updated title',
         'content'=>'updated content',
     ]);
-//使用save方法
-    $post=Post::find(1);
-    $post ->title='saved title';
-    $post ->content='saved content';
-    $post ->save();
+    //使用save方法
+    $post = Post::find(12);
+    $post->title = 'saved title';
+    $post->content = 'saved content';
+    $post->save();
+    */
+    //使用delete方法
+    $post = Post::find(11);
+    $post->delete();
+    //使用destroy方法
+    Post::destroy(12);
+    //刪除多筆資料
+    Post::destroy(13,14,15);
+    //return view('welcome');
 });
 
 Route::get('posts',[PostsController::class,'index'])->name('posts.index');
 Route::get('post',[PostsController::class,"show"])->name('posts.show');
 Route::get('about',[PostsController::class,'about'])->name('posts.about');
-
 Route::get('contact',[PostsController::class,'contact'])->name('posts.contact');
-
-
