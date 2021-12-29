e97339d1561ea4de9ca574a484ce7b6c0a7a1b6e
LAPTOP-Q4E3DFIH\s9633 <s3a832070@student.ncut.edu.tw>
2021-12-29 10:00:47 +0800

    練習 2：新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,11 +14,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
-Route::get('post', [\App\Http\Controllers\PostsController::class, 'show'])->name('posts.show');
-Route::get('contact', [\App\Http\Controllers\PostsController::class, 'contact'])->name('posts.contact');
-Route::get('about', [\App\Http\Controllers\PostsController::class, 'about'])->name('posts.about');
+Route::get('posts',[PostsController::class,'index'])->name('posts.index');
+Route::get('post',[PostsController::class,"show"])->name('posts.show');
+Route::get('about',[PostsController::class,'about'])->name('posts.about');
+
+Route::get('contact',[PostsController::class,'contact'])->name('posts.contact');
+
+$post=new Post();//先產生Post的物件$post，$post代表資料表posts的一篇貼文
+$post->title='test title';//指定貼文的title
+$post->content='test content';//指定貼文的content
+$post->save();
+//使用create方法，P.S 應該會遇到 Mass Assignment 的警告
+Post::create([
+    'title'=>'test title',
+    'content'=>'test content',
+]);
